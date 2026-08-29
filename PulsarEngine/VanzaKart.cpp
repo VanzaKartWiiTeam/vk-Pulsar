8bc88f85a4c51084640bc1cd3e4bf9f27cbf1d0a
Weebruzz <ahmettr3833@gmail.com>
2026-08-29 20:05:38 +0200

    Add files via upload

--- PulsarEngine/VanzaKart.cpp
+++ PulsarEngine/VanzaKart.cpp
@@ -2,8 +2,12 @@
 #include <PulsarSystem.hpp>
 #include <runtimeWrite.hpp>
 #include <Dolphin/DolphinIOS.hpp>
+#include <VanzaKart.hpp>
 
 namespace VanzaKart {
+
+// Mission Mode Fix flag - controls transmission system
+u16 U16_MISSION_MODE_FIX = 0;
 
 // Hide the channel button on the title screen / main menu on emulator only
 kmRuntimeUse(0x80625E1C);
@@ -14,6 +18,18 @@ void HideChannelButton() {
     }
 }
 static SectionLoadHook hideChannelButton(HideChannelButton);
+
+// Initialize transmission array or sum idk lol
+Pulsar::System* System::Create() {
+    System* system = new System();
+    
+    // Initialize all transmissions to default
+    for(int i = 0; i < 12; ++i) {
+        system->transmissions[i] = TRANSMISSION_DEFAULT;
+    }
+    
+    return system;
+}
 
 } // namespace VanzaKart
 
